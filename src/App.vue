68ad5a55bb53ac66f704ca8e8ebe56938a572a45
talhatonmoy <talha.tonmoy.ck@gmail.com>
2023-08-14 12:01:24 +0600

    somewhat upated

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,50 @@
 <script setup>
-import { ref, onMounted, nextTick } from 'vue'
-
-
-// const images = ref([
-//     "https://images.unsplash.com/photo-1691523103943-d48a90280dd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
-//     "https://images.unsplash.com/photo-1691380302078-3d0a8b5fe52d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-//     "https://images.unsplash.com/photo-1675725399060-b638b1c15b50?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1674&q=80",
-//     "https://images.unsplash.com/photo-1688643188950-dcc91eea00de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
-//     "https://images.unsplash.com/photo-1691249238710-8ee787d4fd0a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-// ])
+import { ref, onMounted, nextTick, onBeforeMount } from 'vue'
 
 
 const images = ref([
-    'img/1.avif',
-    'img/2.avif',
-    'img/3.avif',
-    'img/4.avif',
+    "https://plus.unsplash.com/premium_photo-1675804669860-9e27f22b0681?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1650&q=80",
+    "https://images.unsplash.com/photo-1537346439163-eafb59bdc400?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+    "https://plus.unsplash.com/premium_photo-1675448891094-1c3899f6f67a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1539634262233-7c0b48ab9503?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1425&q=80",
 ])
 
-const newImage = ref(null);
 
-//initializing carousel
+// const images = ref([
+//     'img/1.avif',
+//     'img/2.avif',
+//     'img/3.avif',
+//     'img/4.avif',
+// ])
+
 onMounted(() => {
-    const slider = new Flickity('.carousel', {});
+    slider = new Flickity('.carousel', {
+        wrapAround: true
+    });
 })
 
+const newImage = ref(null);
+let slider = null
+
 function addNewImage() {
-    images.value.push(newImage.value)
-    slider.destroy()
-    nextTick(() => {
-        const slider = new Flickity('.carousel', {});
-    })
+    if (newImage.value) {
+        images.value.push(newImage.value)
+        slider.destroy()
+        nextTick(() => {
+            slider = new Flickity('.carousel', {
+                wrapAround: true
+            });
+        })
+        newImage.value = ''
+    }
+    
 }
+
 
 </script>
 
 <template>
-    <div class="container dark:bg-white h-screen p-3">
+    <div class="container dark:bg-white min-h-screen p-3">
         <div class="w-[1200px] ">
             <div class="flex items-center justify-center mb-10 p-3 shadow-lg bg-gray-100 mx-auto space-x-2">
                 <input v-model="newImage" class="px-3 py-1.5 focus:outline-none rounded-sm bg-white text-md text-gray-500 "
@@ -46,11 +54,9 @@ function addNewImage() {
                     Image</button>
             </div>
         </div>
-
-        <div class="w-1/2 mx-auto">
-            <div class="carousel" data-flickity='{ "wrapAround": true }'>
-                <div v-for="(img, index) in images" :key="index"> <img class="w-full h-fit" :src="img" alt=""></div>
-            </div>
+        <p>{{ newImage }}</p>
+        <div class="carousel">
+            <div v-for="img, index in images" :key="index" :style="`background:url(${img})`" class=" w-[650px] h-[450px] bg-contain mx-auto bg-center "></div>
         </div>
     </div>
 </template>
