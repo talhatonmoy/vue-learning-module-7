7b4c0bde50ab827f709ede3e9a781a422912de36
talhatonmoy <talha.tonmoy.ck@gmail.com>
2023-10-15 00:06:55 +0600

    button component with props and default value of props

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,14 @@
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue'
-
-const newValue = ref(null)
-
-let theChart = null
-
-const dataset = [
-    300, 50, 100
-]
-
-const data = {
-    labels: [
-        "Red",
-        "Blue",
-        "Yellow"
-    ],
-    datasets: [
-        {
-            label: 'My first Dataset',
-            data: dataset,
-            backgroundColor: [
-                'rgb(255,99,132)',
-                'rgb(54,162,235)',
-                'rgb(255,205,86)',
-                'rgb(43,105,86)',
-                'rgb(21,21,186)'
-            ],
-            hoverOffset: 4
-        }
-    ]
-}
-
-const config = {
-    type: 'pie',
-    data: data
-}
-
-onMounted(() => {
-    const context = document.getElementById('myChart')
-    theChart = new Chart(context, config)
-})
-
-onBeforeUnmount(() => {
-    if (theChart) {
-        theChart.destroy()
-    }
-})
-
-function updateChart() {
-    if (newValue.value) {
-        dataset.push(newValue.value)
-        theChart.data.datasets[0].data = dataset
-        theChart.update()
-    }
-}
-
-
-
-
+import Button from './components/Button.vue';
 </script>
 
 <template>
-    <div class="w-[500px] h-[500px] border-gray-800 border p-2 mx-auto mb-5">
-          <canvas id="myChart"></canvas>
-    </div>
 
-    <div class="flex items-center justify-center mb-10 p-3 shadow-lg bg-gray-100 mx-auto space-x-2">
-        <input  class="px-3 py-1.5 focus:outline-none rounded-sm bg-white text-md text-gray-500 " v-model="newValue"
-            type="text" placeholder="Input value">
-        <button @click="updateChart()"  class="px-3 py-1.5 bg-blue-600 text-white border-none rounded-sm hover:bg-blue-800"
-            type="submit">Add
-            To Chart</button>
-    </div>
+    <Button label="Click Me" color="slate"/>
+    <hr class="my-4">
+    <Button label="Download" color="red"/>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+    label: {
+        type: String,
+        default: 'Hello World!'
+    },
+    color: {
+        type: String,
+        default: 'yellow'
+    }
+})
+
+function getColor() {
+    return ` bg-${props.color}-800  hover:bg-${props.color}-400`
+}
+</script>
+
+<template>
+    <button :class="getColor()" class="px-6 py-3 text-white font-bold hover:text-black rounded-md">
+        {{ props.label }}
+    </button>
+</template>
+
+<style scoped></style>
